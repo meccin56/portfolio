04f94e2bfa46e8fc65a6ee5909053ade1ea607f9
meccin56 <meccin56@gmail.com>
2024-05-03 00:19:08 +0900

    no message

--- 2tax/script.js
+++ 2tax/script.js
@@ -15,18 +15,27 @@ const ugoki={
 btn.animate(ugoki,1200);
 
 
+const Menu = document.querySelector('.menu');
+const Hum = document.querySelector('#hum');
+const Batu = document.querySelector('#batu');
+const Ul = document.querySelector('ul');
+
+Menu.addEventListener('click',()=>{
+	Hum.classList.toggle('active');
+	Batu.classList.toggle('active');
+	Ul.classList.toggle('active');
+});
+
+
+// const Icon = document.querySelector('.icon');
+// const Hum = document.querySelector('.hum');
+// const Batu = document.querySelector('.batu');
+// const Menu=document.querySelector('.menu');
 
 
 
-const Icon = document.querySelector('.icon');
-const Hum = document.querySelector('.hum');
-const Batu = document.querySelector('.batu');
-const Menu=document.querySelector('.menu');
-
-
-
-Icon.addEventListener('click', () => {
-		Hum.classList.toggle('active');
-		Batu.classList.toggle('active');
-		Menu.classList.toggle('active');
-	});
+// Icon.addEventListener('click', () => {
+// 		Hum.classList.toggle('active');
+// 		Batu.classList.toggle('active');
+// 		Menu.classList.toggle('active');
+// 	});
