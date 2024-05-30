b7f1f92734f180b35d9c572da3ee39da6a5a63cd
meccin56 <meccin56@gmail.com>
2024-05-31 05:53:53 +0900

    taxを追加

--- tax/script.js
+++ tax/script.js
@@ -1,32 +1,33 @@
-// const heading =document.querySelector('#main-message1');
+const Menu = document.querySelector('.menu');
+const Hum = document.querySelector('#hum');
+const Batu = document.querySelector('#batu');
+const Ul = document.querySelector('ul');
 
-// const keyframes = {
+Menu.addEventListener('click',()=>{
+	Hum.classList.toggle('active');
+	Batu.classList.toggle('active');
+	Ul.classList.toggle('active');
+});
+
+
+// const btn=document.querySelector('#main-btn');
+// const ugoki={
 //     opacity:[0,1],
-//     translate:['100px 0px',0],
-// };
-// heading.animate(keyframes,800);
+//     translate:['-160px 0px',0],
+// 	easing:'ease',
+// }
+// btn.animate(ugoki,1200);
 
-const btn=document.querySelector('#main-btn');
-const ugoki={
-    opacity:[0,1],
-    translate:['-160px 0px',0],
-	easing:'ease',
-}
-btn.animate(ugoki,1200);
 
+// const Icon = document.querySelector('.icon');
+// const Hum = document.querySelector('.hum');
+// const Batu = document.querySelector('.batu');
+// const Menu=document.querySelector('.menu');
 
 
 
-
-const Icon = document.querySelector('.icon');
-const Hum = document.querySelector('.hum');
-const Batu = document.querySelector('.batu');
-const Menu=document.querySelector('.menu');
-
-
-
-Icon.addEventListener('click', () => {
-		Hum.classList.toggle('active');
-		Batu.classList.toggle('active');
-		Menu.classList.toggle('active');
-	});
+// Icon.addEventListener('click', () => {
+// 		Hum.classList.toggle('active');
+// 		Batu.classList.toggle('active');
+// 		Menu.classList.toggle('active');
+// 	});
