9072cfa1534d8e657a9c47f89f95f3841eac917d
meccin56 <meccin56@gmail.com>
2024-08-25 09:28:53 +0900

    no message

--- action/script.js
+++ action/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+//モーダル1
 {
     const open= document.getElementById('open');
     const modal= document.getElementById('modal');
@@ -22,12 +23,30 @@
         modal.classList.add('hidden');
         mask.classList.add('hidden');
     });
-
-
-
 }
 
+//モーダル2
+{
+    const open2 = document.getElementById('open2');
+    const close2 = document.getElementById('close2');
+    const mask2 = document.getElementById('mask2');
+    const modal2 = document.getElementById('modal2');
 
+    open2.addEventListener('click', ()=>{
+        modal2.classList.remove('hidden2');
+        mask2.classList.remove('hidden2');
+    });
+
+    close2.addEventListener('click',()=>{
+        modal2.classList.add('hidden2');
+        mask2.classList.add('hidden2');
+    })
+
+    mask2.addEventListener('click', ()=>{
+        modal2.classList.add('hidden2');
+        mask2.classList.add('hidden2');
+    });
+}
 
 
 
