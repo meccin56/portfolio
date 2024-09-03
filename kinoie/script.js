447447689c2f3d9ce6dd90b3b548482bc73956f2
meccin56 <meccin56@gmail.com>
2024-09-03 14:35:01 +0900

    no message

--- kinoie/script.js
+++ kinoie/script.js
@@ -1,18 +1,16 @@
-
+// オープニング
 const heading =document.querySelector('#heading');
-
 const keyframes = {
     opacity:[0,1],
     translate:['0 50px',0],
 };
-
 const option = {
     duration:3000,
     easing:'ease',
 }
-
 heading.animate(keyframes,option);
 
+// 建築事例
 const ca = document.querySelectorAll('.cases');
 
 // console.log(ca);
@@ -30,3 +28,27 @@ for(let i = 0; i < ca.length; i++){
 
 ca[i].animate(keyframes, options);
 }
+
+
+
+// 減税制度
+// 監視対象が範囲内に現れたら実行する動作
+const genzei = (entries) => {
+
+    const key = {
+      opacity: [0, 1],
+    };
+    const op = {
+        duration:2000,
+        fill:'forwards',
+    };
+
+    entries[0].target.animate(key, op);
+  };
+    
+  // 監視ロボットの設定
+  const genzeiObserver = new IntersectionObserver(genzei);
+  
+  // #kirinを監視するよう指示
+  genzeiObserver.observe(document.querySelector('.tax-content'));
+
