4745e93ca71ba5e1e5bb1785b6775f6d646a5aa1
meccin56 <meccin56@gmail.com>
2024-10-16 12:16:34 +0900

    no message

--- manabi/script13.js
+++ manabi/script13.js
@@ -1,7 +1,7 @@
 const loaderContainer = document.querySelector('.loader-container');
 const section = document.querySelector('.section');
 
-window.addEventListener('load', () => {
+window.addEventListener('click', () => {
   loaderContainer.classList.add('hidden');
   section.classList.add('visible');
 });
