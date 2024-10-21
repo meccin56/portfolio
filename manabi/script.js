764ecc8b5dfcd2b0a53ce60d403014adc4ac03f5
meccin56 <meccin56@gmail.com>
2024-10-21 13:37:07 +0900

    カレンダー

--- manabi/script.js
+++ manabi/script.js
@@ -14,68 +14,7 @@ dateElement.textContent = `今日は${year}年${month}月${day}日です`;
 
 
 
-// 配列  ----------------------------------------------------
-const Items = document.querySelector('.items');
-const Lists = [
-    {
-        name: 'プランA',
-        price: '3000',
-        star: '☆☆☆☆',
-        img: '022.jpg',
-    },
-    {
-        name: 'プラン1',
-        price: '2000',
-        star: '☆☆☆',
-        img: '021.jpg',
-    },
-    {
-        name: 'プランC',
-        price: '1000',
-        star: '☆☆',
-        img: '020.jpg',
-    },
-    {
-        name: 'プランD',
-        price: '8000',
-        star: '☆☆☆☆',
-        img: '019.jpg',
-    },
-    {
-        name: 'プランE',
-        price: '9000',
-        star: '☆☆☆☆☆',
-        img: '018.jpg',
-    },
-    {
-        name: 'プランF',
-        price: '4000',
-        star: '☆☆☆',
-        img: '017.jpg',
-    },
-    {
-        name: 'プランG',
-        price: '4000',
-        star: '☆☆☆',
-        img: '014.jpg',
-    },
-    {
-        name: 'プランH',
-        price: '4000',
-        star: '☆☆☆',
-        img: '015.jpg',
-    },
-];
 
-for(let i = 0; i < Lists.length; i++){
-const Content = `<div>
-<img src="img/${Lists[i].img}">
-<h4>${Lists[i].name}</h4>
-<p>${Lists[i].price}円</p>
-<p>オススメ：${Lists[i].star}</p>
-</div>`;
-Items.insertAdjacentHTML('beforeend',Content);
-}
 
 
 
